fb52000e70e935c73f060c38518c80497d72ed80
20201tiimi0136 <lavinianina@outlook.com>
2022-09-16 11:58:37 -0300

    projeto galeria

---
+++ app/src/main/java/mariano/lavinia/galeria/MeuViewHolder.java
@@ -0,0 +1,11 @@
+package mariano.lavinia.galeria;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MeuViewHolder extends RecyclerView.ViewHolder {
+    public MeuViewHolder(View intenView) {
+        super(intenView);
+    }
+}
